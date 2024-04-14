7169e1d37f6d0b2ac5ceb62ac6b82c463fa335f4
Islam <it.kaktus312@gmail.com>
2024-04-14 22:30:04 +0600

    Converted to JSON Web Token

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from apps.users.views import RegisterAPIView, LoginAPIView, LogoutAPIView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from apps.users.views import RegisterAPIView, LoginAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
-    path('logout/', LogoutAPIView.as_view(), name='logout')
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,9 @@
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import CustomUser
 from .serializers import RegisterSerializer, LoginSerializer
@@ -15,9 +15,9 @@ class RegisterAPIView(APIView):
         serializer.is_valid(raise_exception=True)
 
         user = CustomUser.objects.create_user(**serializer.validated_data)
-        token, created = Token.objects.get_or_create(user=user)
+        refresh = RefreshToken.for_user(user)
 
-        return Response({'token': token.key, 'data': serializer.data},
+        return Response({'refresh': str(refresh), 'access': str(refresh.access_token), 'data': serializer.data},
                         status=status.HTTP_201_CREATED)
 
 
@@ -27,18 +27,12 @@ class LoginAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         user = authenticate(**serializer.validated_data)
         if user:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'message': 'Successfully logged in', 'token': token.key},
-                            status=status.HTTP_200_OK)
-        return Response({'message': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class LogoutAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        request.user.auth_token.delete()
-        return Response(
-            {'message': 'You have been successfully logged out.'},
-            status=status.HTTP_200_OK
-        )
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'message': 'Successfully logged in',
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }, status=status.HTTP_200_OK)
+        return Response({
+            'message': 'Invalid credentials'
+        }, status=status.HTTP_400_BAD_REQUEST)
